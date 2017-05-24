c2796ced4728844af1e4f84f4961366739b3fe05
Nicolas M. Thiéry <nthiery@users.sf.net>
2017-05-23 23:42:57 -0400

    Added support for subspaces of polynomials, ...

--- code.py
+++ code.py
@@ -1,5 +1,73 @@
 import functools
 
+def items_of_vector(v):
+    """
+    Return an iterator over the pairs ``(index, coefficient)`` for `v`.
+
+    INPUT::
+
+    - ``v`` -- an element of some some vector space or free module
+
+    EXAMPLES:
+
+    This handles indexed free module elements::
+
+        sage: E = CombinatorialFreeModule(QQ, [1,2,4,8,16])
+        sage: v = E.an_element(); v
+        2*B[1] + 2*B[2] + 3*B[4]
+        sage: list(items_of_vector(v))
+        [(1, 2), (2, 2), (4, 3)]
+
+    free module elements::
+
+        sage: v = vector([4,0,1,2])
+        sage: list(items_of_vector(v))
+        [(0, 4), (2, 1), (3, 2)]
+
+        sage: v = vector([4,0,1,2], sparse=True)
+        sage: list(items_of_vector(v))
+        [(0, 4), (2, 1), (3, 2)]
+
+    multivariate polynomials::
+
+        sage: P = QQ['x,y,z']
+        sage: x,y,z = P.gens()
+        sage: p = (x+y+1)^2; p
+        x^2 + 2*x*y + y^2 + 2*x + 2*y + 1
+        sage: list(items_of_vector(p))
+        [((1, 0, 0), 2),
+         ((1, 1, 0), 2),
+         ((0, 0, 0), 1),
+         ((2, 0, 0), 1),
+         ((0, 1, 0), 2),
+         ((0, 2, 0), 1)]
+
+    univariate polynomials::
+
+        sage: P = ZZ['x']
+        sage: x = P.gen()
+        sage: (x+2)^3
+        x^3 + 6*x^2 + 12*x + 8
+        sage: list(items_of_vector(_))
+        [(0, 8), (1, 12), (2, 6), (3, 1)]
+
+    elements of quotients::
+
+        sage: C = CyclotomicField(5)
+        sage: z = C.gen()
+        sage: p = (z+2)^2; p
+        zeta5^2 + 4*zeta5 + 4
+        sage: list(items_of_vector(p))
+        [(0, 4), (1, 4), (2, 1)]
+    """
+    if isinstance(v, CombinatorialFreeModule.Element):
+        return v
+    else:
+        try:
+            return v.dict().items()
+        except AttributeError:
+            return items_of_vector(v.lift())
+
 class Subspace:
     """
     Construct a subspace from generators and linear operators
@@ -41,6 +109,27 @@ class Subspace:
         [ 0  0  1  0 -1]
         [ 0  0  0  1  1]
 
+    Computing a subspace of a multivariate polynomial ring::
+
+        sage: P = QQ['x,y,z']
+        sage: x,y,z = P.gens()
+        sage: F = Subspace([x-y, y-z, x-z])
+        sage: F.dimension()
+        2
+        sage: F._matrix
+        [ 1  0 -1]
+        [ 0  1 -1]
+
+    The derivatives of the Van-der-Monde determinant in `n` variables
+    spans a space of dimension `n!`::
+
+        sage: Delta = (x-y)*(y-z)*(x-z)
+        sage: F = Subspace([Delta], [attrcall("derivative", x) for x in P.gens()])
+        sage: F.dimension()
+        6
+
+    Computing subalgebras and modules in the algebra of the symmetric
+    group::
 
         sage: S = SymmetricGroup(4)
         sage: A = S.algebra(QQ)
@@ -106,7 +195,7 @@ class Subspace:
         # - implement and use a generic api to recover the items
         assert v.base_ring() == self._base_ring
         rank = self._rank
-        d = dict((rank(i), c) for i, c in v)
+        d = dict((rank(i), c) for i, c in items_of_vector(v))
         return vector(self._base_ring, len(self._rank.cache), d)
 
     def _extend(self, v):
