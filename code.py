e3912db56783c3d144dc80026379fdf8b896abb6
Nicolas M. Thiéry <nthiery@users.sf.net>
2017-05-23 22:48:07 -0400

    Implementation of module closure under linear operators

---
+++ code.py
@@ -0,0 +1,137 @@
+class Subspace:
+    """
+    Construct a subspace from generators and linear operators
+
+    INPUT:
+
+    - ``generators`` -- a list of vectors in some ambient vector space `V`
+    - ``operators`` -- a list of linear endomorphism `V`
+
+    Return the smallest subspace of `V` containing ``generators`` and
+    stable under the action of the operators.
+
+    EXAMPLES::
+
+        sage: E = CombinatorialFreeModule(QQ, [1,2,4,8,16])
+        sage: v = E.an_element(); v
+        2*B[1] + 2*B[2] + 3*B[4]
+        sage: F = Subspace([v, v], [])
+        sage: F.dimension()
+        1
+
+        sage: F = Subspace([B[1]-B[2], B[2]-B[4], B[1]-B[4]])
+        sage: F.dimension()
+        2
+        sage: F._matrix
+        [ 1  0 -1]
+        [ 0  1 -1]
+
+        sage: E = CombinatorialFreeModule(QQ, [1,2,4,8,16])
+        sage: B = E.basis()
+        sage: phi = E.module_morphism(lambda i: B[i]+B[2*i] if i <= 8 else E.zero(), codomain=E)
+        sage: F = Subspace([phi(B[1])], [phi])
+        sage: F.dimension()
+        4
+        sage: F._matrix
+        [ 1  0  0  0 -1]
+        [ 0  1  0  0  1]
+        [ 0  0  1  0 -1]
+        [ 0  0  0  1  1]
+
+
+        sage: F = Subspace([A.one()], [functools.partial(operator.mul, A.jucys_murphy(i)) for i in range(1,4)])
+        sage: F.dimension()
+        4
+        sage: F._matrix
+        [1 0 0 0 0 0]
+        [0 1 1 0 0 0]
+        [0 0 0 1 1 0]
+        [0 0 0 0 0 1]
+
+        sage: S = SymmetricGroup(4)
+        sage: A = S.algebra(QQ)
+        sage: P = Partitions(4)
+        sage: def young_idempotent(p):
+        ....:     return A.sum_of_terms((S(sigma), sigma.sign()) for sigma in p.column_stabilizer()) * A.sum_of_monomials(S(sigma) for sigma in p.row_stabilizer())
+
+        sage: for p in P:
+        ....:     print p.shape(), p.shape().dimension(), Subspace([young_idempotent(p)], [functools.partial(operator.mul, s) for s in A.algebra_generators()]).dimension()
+        [4] 1 1
+        [3, 1] 3 3
+        [3, 1] 3 3
+        [3, 1] 3 3
+        [2, 2] 2 2
+        [2, 2] 2 2
+        [2, 1, 1] 3 3
+        [2, 1, 1] 3 3
+        [2, 1, 1] 3 3
+        [1, 1, 1, 1] 1 1
+    """
+
+    def __init__(self, generators, operators=[]):
+        self._ambient = generators[0].parent()
+        self._base_ring = self._ambient.base_ring()
+        self._generators = generators
+        self._operators = operators
+        self._rank, self._unrank = sage.combinat.ranker.on_fly()
+        self._matrix = matrix(self._ambient.base_ring(), 0, 0)
+        generators = [v
+                      for v in generators
+                      if self._extend(v)]
+        self._todo = [(v, op)
+                      for v in generators
+                      for op in operators]
+        self.finalize()
+
+    def vector(self, v):
+        """
+        Return `v` as a plain vector
+
+        INPUT:
+
+        - ``v`` -- an element of the ambient space
+
+        Invariant: when it's returned, the length of the vector is the
+        number of basis elements ranked, which is at least the number
+        of columns of the matrix.
+        """
+        # TODO:
+        # - optimize this
+        # - implement and use a generic api to recover the items
+        assert v.base_ring() == self._base_ring
+        rank = self._rank
+        d = dict((rank(i), c) for i, c in v)
+        return vector(self._base_ring, len(self._rank.cache), d)
+
+    def _extend(self, v):
+        m = self._matrix
+        dim_before = m.nrows()
+        r = self.vector(v)
+        if len(r) > m.ncols():
+            m = m.augment(matrix(self._base_ring, m.nrows(), len(r)-m.ncols()))
+        m = m.stack(r)
+        m.echelonize()
+        if m[-1]:
+            self._matrix = m
+            return True
+        return False
+
+    def _echelonize():
+        self._matrix.echelonize()
+
+    @cached_method
+    def dimension(self):
+        """
+
+        """
+        self.finalize()
+        return self._matrix.nrows()
+
+    @cached_method
+    def finalize(self):
+        todo = self._todo
+        while todo:
+            v,op = todo.pop()
+            w = op(v)
+            if self._extend(w):
+                todo.extend((w,op) for op in self._operators)
