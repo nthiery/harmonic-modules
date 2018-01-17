784a312ce171c506463838156ae01f6389cea188
Nicolas M. Thiéry <nthiery@users.sf.net>
2018-01-17 09:59:13 +0100

    Debriefing after the talk
    - Added a bunch of TODO's
    - Separate file for the output customization
    - Use unicode_art by default (more robust then mathjax for now)

---
+++ talk.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Ideally, we would put this in an ".ipy" file to be able to use magic
+# commands, but this causes trouble with the unicode character below
+
+# %run "code.py"
+# %display unicode_art
+
+tensor.symbol=u" ⊗ "
+Partitions.options.convention = "french"
+Partitions.options.display = "list"
+from sage.typeset.unicode_art import UnicodeArt
+Partition._unicode_art_ = lambda p: UnicodeArt([''.join(str(i) for i in p)])
