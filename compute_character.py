8ee3e5f521dccc4068e053b0969ce4e0af7333b6
phubert <hubert.pauline@courrier.uqam.ca>
2019-05-03 15:29:33 +0200

    Clean, add last functions and optimisation and add documentation

--- compute_character.py
+++ compute_character.py
@@ -94,29 +94,14 @@ def steenrod_operators(P, degree=1):
     MATH..:: \sum_i x_i \partial_{x_i}^{k+1}
     
     INPUT: 
-    - ``P`` -- a polynomial ring and degree an interger. 
+    - ``P`` -- a polynomial ring
+    - ``degree`` -- an Interger. 
     """
     r = P._r
     D = P._grading_set
     op = {}
     for i in range(r):
         op[D((-degree if j==i else 0 for j in range(r)))] = [functools.partial(P.steenrod_op, i=i, k=degree+1)]
-    return op
-
-def diagonal_steenrod_operators(P, list_deg):
-    """
-    Return the diagonal Steenrod operators
-    
-    INPUT:
-    - ``P`` -- a polynimial ring in at least two sets of variables
-    - ``list_deg`` -- a list of the degree for the operators
-    """
-    r = P._r
-    D = P._grading_set
-    op = {}
-    for dx, dy in list_deg:
-        op[D((-dx if j==0 else -dy if j==1 else 0 for j in range(r)))] = [
-            functools.partial(P.diagonal_steenrod, i1=0, i2=1, d1=dx, d2=dy)]
     return op
 
 def polarization_operators(P, side=None, row_symmetry=None, max_deg=0):
@@ -176,6 +161,7 @@ def higher_polarization_operators(P, side=None, row_symmetry=None, max_deg=0):
             if (i1<i2 if side == 'down' else i1!=i2)
            }
 
+
 def symmetric_derivatives(P, list_deg, row_symmetry=None):
     """
     Return the list of all symmetric derivative of degree contained
@@ -191,6 +177,19 @@ def symmetric_derivatives(P, list_deg, row_symmetry=None):
   
 
 def merge(dict1, dict2):
+    """
+    Merge two dictionnaries whose values are lists into the first one. 
+    
+    INPUT:
+    - ``dict1``, ``dict2`` -- dictionnaries
+    
+    EXAMPLES::
+        sage: dict1 = {"colors":["red", "blue"], "numbers":[1,2,3]}
+        sage: dict2 = {"colors":["blue", "green"]}
+        sage: merge(dict1, dict2)
+        {'colors': ['red', 'blue', 'blue', 'green'], 'numbers': [1, 2, 3]}
+
+    """
     result = dict1
     for key, value in dict2.iteritems():
         if key in result:
@@ -198,13 +197,43 @@ def merge(dict1, dict2):
         else:
             result[key] = value
     return result
+
+def antisymmetries(mu):
+    """
+    Return the antisymmetries associated to the canonical stantard tableau
+    of the given partition. 
     
+    INPUT:
+    - ``mu`` -- a Partition
+    
+    EXAMPLES ::
+        sage: antisymmetries(Partition([2,1]))
+        ((0, 2), (1,))
+        sage: antisymmetries(Partition([3]))
+        ((0,), (1,), (2,))
+        sage: antisymmetries(Partition([4,2,1,1]))
+        ((0, 4, 6, 7), (1, 5), (2,), (3,))
+
+    """
+    mu = Partition(mu)
+    return antisymmetries_of_tableau(mu.initial_tableau())
     
 ##############################################################################
 # Projection on isotypic components
 ##############################################################################
 
 def Isotyp(S, arg):
+    """
+    Project the basis of the given subspace S on the isotypic components of $S_n$
+    or on the isotypic component of the given type. 
+    
+    INPUT:
+        -``S`` -- Subspace
+        -``arg`` -- an integer or a partition
+        
+    EXAMPLES::
+    
+    """
     if isinstance(arg, Partition):
         list_partitions = [arg]
     elif isinstance(arg, Integer):
@@ -222,55 +251,97 @@ def Isotyp(S, arg):
                 result[(key, tuple(nu))] = basis_nu[0]
     return Subspace(result, operators={})
 
-def Isotyp2(S, arg):
-    if isinstance(arg, Partition):
-        list_partitions = [arg]
-    elif isinstance(arg, Integer):
-        list_partitions = Partitions(arg)
-    else : 
-        print("Error: arg should be a partition or an integer.")
+
+def add_degrees_isotypic(d1, d2):
+    """
+    Compute the sum componentwise of the lists of integrers contained in d1 and d2 
+    and return a grading set and the partition contained in d2 as result.
     
-    basis = S.basis()
-    result = {}
-    for nu in list_partitions:
-        for key, value in basis.iteritems():
-            gen = [apply_young_idempotent(p, nu) for p in value]
-            basis_nu = Subspace(gen, {}).basis()
-            if basis_nu != {} :
-                result[(key[0], tuple(nu))] = basis_nu[0] #key[0] >>> key
-    return Subspace(result, operators={})
+    INPUT:
+        - ``d1`` -- list containing a list of integers and a partition
+        - ``d2`` -- list of integers
 
+    EXAMPLES::
+        sage: d1 = ([3,0],[2,1])
+        sage: d2 = [-1,0]
+        sage: add_degrees_isotypic(d1,d2)
+        ((2, 0), [2, 1])
 
-def add_degrees_isotypic(gen_deg, op_deg):
-    D = cartesian_product([ZZ for i in range(len(gen_deg[0]))])
-    return D(gen_deg[0])+D(op_deg), gen_deg[1]
+    """
+    D = cartesian_product([ZZ for i in range(len(d1[0]))])
+    return D(d1[0])s+D(d2), d1[1]
+    
+def add_degrees_symmetric(d1,d2):
+    """
+    Compute the sum componentwise of the lists of integrers contained in d1 and d2 
+    and return an ordered grading set and the partition contained in d2 as result.
+    
+    INPUT:
+        - ``d1`` -- list containing a list of integers and a partition
+        - ``d2`` -- list of integers
 
-def add_degrees_test(gen_deg, op_deg):
-    return gen_deg+op_deg
+    EXAMPLES::
+        sage: d1 = ([0, 3],[2,1])
+        sage: d2 = [0, -1]
+        sage: add_degrees_symmetric(d1,d2)
+        ((2, 0), [2, 1])
+
+    """
+    D = cartesian_product([ZZ for i in range(len(d1[0]))])
+    d = D(d1[0])+D(d2)
+    return D(sorted(d, reverse=True)), d1[1]
     
     
 ##############################################################################
 # Polarization Space
 ##############################################################################
 
-def PolarizedSpace(pol_ring, S, operators):
+def PolarizedSpace(P, S, operators, add_degrees=add_degrees_isotypic):
+    """
+    Polarized the subspace S with the given operators on the polynomial ring P. 
+    
+    INPUT:
+    - ``P`` -- a polynomial ring
+    - ``S`` -- a subspace
+    - ``operators`` -- a list or a dictionnary of operators acting on elements of P
+    - ``add_degrees`` -- a function that will be used to determine the degrees of the elements computed
+    
+    EXAMPLES::
+    """
     basis = S.basis()
-    P = pol_ring
+    n = P._n
     r = P._r
+    inert = P._inert
     D = P._grading_set
-    
     generators = {}
-    for key, value in basis.iteritems():
-        d = (D((key[0][0] if i==0 else 0 for i in range(0,r))), key[1])
-        generators[d] = [P(b) for b in value]
-    return Subspace(generators, operators, add_degrees=add_degrees_isotypic)
     
+    if isinstance(P, DiagonalAntisymmetricPolynomialRing):
+        antisymmetries = P._antisymmetries
+        for key, value in basis.iteritems():
+            d = (D((key[0][0] if i==0 else 0 for i in range(0,r))), key[1])
+            generators[d] = [antisymmetric_normal(P(b), n, r+inert, antisymmetries) for b in value]
+    else :
+        for key, value in basis.iteritems():
+            d = (D((key[0][0] if i==0 else 0 for i in range(0,r))), key[1])
+            generators[d] = [P(b) for b in value]
+            
+    return Subspace(generators, operators, add_degrees=add_degrees)  
     
 ##############################################################################
 # Quotient
 ##############################################################################
 
 def Range(S, operators, add_degrees=add_degrees_isotypic):
+    """
+    Return the image of the basis of the subspace S by the given operators. 
+    
+    INPUT:
+    - ``S`` -- a subspace
+    - ``operators`` -- a list or a dictionnary of operators acting on elements of P
+    - ``add_degrees`` -- a function that will be used to determine the degrees of the elements computed
+    
+    EXAMPLES::
+    """
     result = {}
     basis = S.basis()
     for key, b in basis.iteritems():
@@ -278,7 +349,7 @@ def Range(S, operators, add_degrees=add_degrees_isotypic):
                                      [op(p) for p in b for op in op_list if op(p)!=0] 
                                      for deg, op_list in operators.iteritems()})    
     if result != {} :
-        return Subspace(result, {}, add_degrees) #{} <-> operators
+        return Subspace(result, {}, add_degrees)
     else :
         return None
         
@@ -288,6 +359,19 @@ def Range(S, operators, add_degrees=add_degrees_isotypic):
 ##############################################################################
 
 def character(S, n, r, left_basis=s, right_basis=s, row_symmetry=None):
+    """
+    Return the bicharacter of the subspace `S` into the given bases. The subspace `S`
+    must be a multivariate polynomial subspace on `r` sets of `n` variables. 
+    
+    INPUT:
+    - ``S`` -- a subspace
+    - ``n``, ``r`` -- integers
+    - ``left_basis`` -- a basis of the symmetric functions for the $GL_r$-character
+    - ``right_basis`` -- a basis of the symmetric functions for the $S_n$-character
+    - ``row_symmetry`` -- use "permutation" to compute using the symmetries on rows
+    
+    EXAMPLES::
+    """
     basis = S.basis()
     charac = 0
     if row_symmetry != "permutation":
@@ -328,6 +412,19 @@ def character(S, n, r, left_basis=s, right_basis=s, row_symmetry=None):
     
     
 def character_quotient(M, N, n, r, left_basis=s, right_basis=s):
+    """
+    Compute the difference of bicharacter between the subspaces `M` and `N`.
+    They have to be subspaces of multivariate polynomials on `r` sets of `n` variables. 
+    
+    INPUT:
+    - ``M``, ``N`` -- subspaces
+    - ``n``, ``r`` -- integers
+    - ``left_basis`` -- a basis of the symmetric functions for the $GL_r$-character
+    - ``right_basis`` -- a basis of the symmetric functions for the $S_n$-character
+    - ``row_symmetry`` -- use "permutation" to compute using the symmetries on rows
+    
+    EXAMPLES::
+    """
     b_tot = M.basis()
     b_ideal = N.basis()
     charac = 0
@@ -394,15 +491,3 @@ def dimension(f, n):
     q = result2[0][1].parent().gens()[0]
     return [(tuple(a), b.subs({q:1})) for a,b in result2]
 
-def dimension2(f, n):
-    q,t = QQ['q,t'].gens()
-    supp = sorted(f.support())
-    result = {}
-    res = []
-    for mu in Partitions(n):
-        result[mu] = []
-        for (a, b), c in zip(supp, f.coefficients()):
-            if b == mu :
-                result[mu] += [(a,c)]
-    result2 = [(mu,sum(c*s(nu) for (nu,c) in result[mu]).expand(2, alphabet=[q,t])) for mu in result.keys()]
-    return [(tuple(a), b.subs({q:1,t:1})) for a,b in result2]
